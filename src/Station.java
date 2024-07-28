0351a5fa485e5bcaa069adf4319c23b414a78c9e
Kyluuuu <100751079+Kyluuuu@users.noreply.github.com>
2024-07-28 14:27:18 +1000

    Update Station.java

--- src/Station.java
+++ src/Station.java
@@ -3,13 +3,31 @@ import java.util.logging.Logger;
 
 public class Station implements Paintable{
     private static final Logger logger = Logger.getLogger(Station.class.getName());
+    int x = 0;
+    int y = 0;
+    int stationSizeH = 25;
+    int stationSizeW = 50;
+    double angle;
+    enum State {
+        OPENDOOR, CLOSEDOOR, OFF
+    }
+    State stationState = State.OFF;
+
+    public Station(int x, int y, double angle) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        stationState = State.OFF;
+    }
 
     @Override
     public void paint(Graphics g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'paint'");
+        g.drawRect(x, y, stationSizeW, stationSizeH);
+        g.drawString(stationState.name(), x, y);
     }
-    
 
-
+    public void setPos(Point p) {
+        this.x = (int) p.getX();
+        this.y = (int) p.getY();
+    }
 }
