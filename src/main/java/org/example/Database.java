8ad1864ffbd79db107f4887619de5587dccbb00d
vchan <v.chandrapati@outlook.com>
2024-09-06 11:56:49 +1000

    Slightly updated Database

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -6,31 +6,21 @@ import java.util.concurrent.*;
 import java.util.logging.Logger;
 
 public class Database {
-    private static final Logger logger = Logger.getLogger(Server.class.getName());
+    private static final Logger logger = Logger.getLogger(Database.class.getName());
     private static volatile Database db;
-    private BlockingQueue<Runnable> taskQueue;
-    public static ConcurrentHashMap<String, TrainClient> trains;
-    private static ConcurrentHashMap<String, StationClient> stations;
-    private static ConcurrentHashMap<String, CheckpointClient> checkpoints;
-    private static ConcurrentHashMap<String, Integer> trainBlockMap;
-    private static ConcurrentHashMap<String, Integer> stationLocations;
-
-    private static Boolean running = false;
-
-    private ExecutorService executor;
+    private final ConcurrentHashMap<String, TrainClient> trains;
+    private final ConcurrentHashMap<String, StationClient> stations;
+    private final ConcurrentHashMap<String, CheckpointClient> checkpoints;
+    private final ConcurrentHashMap<String, Integer> trainBlockMap;
+    private final ExecutorService executor;
 
     private Database() {
-        running = true;
-
         trains = new ConcurrentHashMap<>();
         stations = new ConcurrentHashMap<>();
         checkpoints = new ConcurrentHashMap<>();
         trainBlockMap = new ConcurrentHashMap<>();
-        stationLocations = new ConcurrentHashMap<>();
 
-        taskQueue = new LinkedBlockingQueue<>();
-
-        executor = Executors.newSingleThreadExecutor();
+        executor = Executors.newCachedThreadPool();
     }
 
     public static synchronized Database getInstance() {
@@ -68,33 +58,16 @@ public class Database {
     }
 
     public Future<TrainClient> getTrain(String id) {
-        return executor.submit(() -> {
-            return trains.get(id);
-        });
+        return executor.submit(() -> trains.get(id));
     }
 
     public Future<StationClient> getStation(String id) {
-        return executor.submit(() -> {
-            return stations.get(id);
-        });
+        return executor.submit(() -> stations.get(id));
     }
 
     public Future<CheckpointClient> getCheckpoint(String id) {
-        return executor.submit(() -> {
-            return checkpoints.get(id);
-        });
+        return executor.submit(() -> checkpoints.get(id));
     }
-
-    // Deprecated function
-    // public CheckpointClient getHit() {
-    // for (CheckpointClient checkpoint : checkpoints.values()) {
-    // if (checkpoint.isTripped()) {
-    // return checkpoint;
-    // }
-    // }
-
-    // return null;
-    // }
 
     public void updateTrainBlock(String traindId, int newBlock) {
         executor.submit(() -> trainBlockMap.put(traindId, newBlock));
@@ -104,39 +77,28 @@ public class Database {
         executor.submit(() -> trainBlockMap.get(traindId));
     }
 
-    // This function is not being used right im pretty sure
     public Future<Boolean> isBlockOccupied(int blockId) {
-        return executor.submit(() -> {
-            return checkpoints.containsValue(blockId);
-        });
+        return executor.submit(() -> trainBlockMap.containsValue(blockId));
     }
 
     public Future<String> getLastTrainInBlock(int blockId) {
-        return executor.submit(() -> {
-            return trainBlockMap.entrySet()
-                    .stream()
-                    .filter(entry -> entry.getValue().equals(blockId))
-                    .map(Map.Entry::getKey)
-                    .reduce((first, second) -> second)
-                    .orElse(null);
-        });
+        return executor.submit(() -> trainBlockMap.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().equals(blockId))
+                .map(Map.Entry::getKey)
+                .reduce((first, second) -> second)
+                .orElse(null));
     }
 
-    public Future<Integer> getTrainCount() {
-        return executor.submit(() -> {
-            return trains.size();
-        });
+    public Integer getTrainCount() {
+        return trains.size();
     }
 
-    public Future<Integer> getStationCount() {
-        return executor.submit(() -> {
-            return stations.size();
-        });
+    public Integer getStationCount() {
+        return stations.size();
     }
 
-    public Future<Integer> getCheckpointCount() {
-        return executor.submit(() -> {
-            return checkpoints.size();
-        });
+    public Integer getCheckpointCount() {
+        return checkpoints.size();
     }
 }
