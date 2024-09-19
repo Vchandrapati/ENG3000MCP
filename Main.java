c11c2029dbba0bbaf092692755d63b5a3c989bb8
Eugene Tshien <tshieneugene@gmail.com>
2024-09-19 12:26:37 +1000

    added a text input for commands

---
+++ Main.java
@@ -0,0 +1,127 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    Integer port = 6666;
+    InetAddress add = null;
+    Integer CCPglobal = 0;
+    Integer Checkglobal = 0;
+
+    Integer numCCPClinets;
+    Integer numcheckClinets;
+
+    ArrayList<CCPClient> CCPclients = new ArrayList<>();
+    ArrayList<CheckpointClient> Checkclients = new ArrayList<>();
+
+    Scanner s = new Scanner(System.in);
+
+    public void Run() {
+        System.out.print("How many CCP clients: ");
+        numCCPClinets = s.nextInt();
+
+        System.out.print("How many checkpoints clients: ");
+        numcheckClinets = s.nextInt();
+
+        Text t = new Text(this);
+        t.display();
+
+        try {
+
+            add = InetAddress.getByName("localhost");
+            for (int i = 0; i < numCCPClinets; i++) {
+                // Starts every CCP off messageing to this adddress
+                String ID = "";
+
+                if (i < 9) {
+                    ID = "BR0" + (i + 1);
+                } else {
+                    ID = "BR" + (i + 1);
+                }
+
+                CCPclients.add(new CCPClient(5001 + CCPglobal, add, port, ID));
+                CCPglobal++;
+            }
+
+            for (int i = 0; i < numcheckClinets; i++) {
+                // Starts every client off messageing to this adddress
+                String ID = "";
+
+                if (i < 9) {
+                    ID = "CP0" + (i + 1);
+                } else {
+                    ID = "CP" + (i + 1);
+                }
+                Checkclients.add(new CheckpointClient(3001 + Checkglobal, add, port, ID));
+                Checkglobal++;
+            }
+
+        } catch (Exception e) {
+            System.out.println("should never be here");
+        }
+
+        // Starts by having every client send a fake connection msg
+        for (int i = 0; i < numCCPClinets; i++) {
+            CCPclients.get(i).sendInitialiseConnectionMsg();
+        }
+
+        // Starts by having every client send a fake connection msg
+        for (int i = 0; i < numcheckClinets; i++) {
+            Checkclients.get(i).sendInitialiseConnectionMsg(i);
+        }
+
+    }
+
+    public void Commands(String input) {
+        String[] inputs = input.split(" ");
+        if (inputs.length == 2 && inputs[0].equals("trip")) {
+            try {
+                Checkclients.get(Integer.parseInt(inputs[1])).sendTRIPMsg();
+            } catch (Exception e) {
+                System.out.println("NO! bad user");
+            }
+
+        }
+
+        if (inputs.length == 1 && inputs[0].equals("ccp")) {
+            String ID = "";
+            if (CCPglobal < 9) {
+                ID = "BR0" + (CCPglobal + 1);
+            } else {
+                ID = "BR" + (CCPglobal + 1);
+            }
+            CCPclients.add(new CCPClient(5001 + CCPglobal, add, port, ID));
+            CCPclients.get(CCPclients.size() - 1).sendInitialiseConnectionMsg();
+            CCPglobal++;
+            numCCPClinets++;
+        }
+
+        if (inputs.length == 1 && inputs[0].equals("cp")) {
+            String ID = "";
+            if (Checkglobal < 9) {
+                ID = "CP0" + (Checkglobal + 1);
+            } else {
+                ID = "CP" + (Checkglobal + 1);
+            }
+            Checkclients.add(new CheckpointClient(3001 + Checkglobal, add, port, ID));
+            Checkclients.get(Checkclients.size() - 1).sendInitialiseConnectionMsg(Checkclients.size() - 1);
+            Checkglobal++;
+            numcheckClinets++;
+        }
+
+        if (input.equals("q")) {
+
+            for (int i = 0; i < numCCPClinets; i++) {
+                CCPclients.get(i).stopListen();
+            }
+
+            for (int i = 0; i < numcheckClinets; i++) {
+                Checkclients.get(i).stopListen();
+            }
+
+            s.close();
+            System.exit(0);
+        }
+    }
+}
---
+++ Text.java
@@ -0,0 +1,107 @@
+
+// Java program to create a blank text 
+// field of definite number of columns.
+import java.awt.event.*;
+import javax.swing.*;
+
+class Text extends JFrame implements ActionListener, KeyListener {
+    // JTextField
+    private JTextField t;
+
+    // JFrame
+    private JFrame f;
+
+    // JButton
+    private JButton b;
+
+    // label to display text
+    private JLabel l;
+
+    private String fullText = "";
+
+    private Main main;
+
+    // default constructor
+    public Text(Main m) {
+        main = m;
+        // create a new frame to store text field and button
+        f = new JFrame("textfield");
+
+        // create a label to display text
+        l = new JLabel("");
+
+        // create a new button
+        b = new JButton("submit");
+
+        // addActionListener to button
+        b.addActionListener(this);
+
+        // create a object of JTextField with 16 columns
+        t = new JTextField(16);
+
+        // add keylistener to textfield
+        t.addKeyListener(this);
+
+        // create a panel to add buttons and textfield
+        JPanel p = new JPanel();
+
+        // add buttons and textfield to panel
+        p.add(t);
+        p.add(b);
+        p.add(l);
+
+        // add panel to frame
+        f.add(p);
+
+        // set the size of frame
+        f.setSize(280, 300);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setAlwaysOnTop(true);
+        f.setResizable(false);
+    }
+
+    public void display() {
+        f.setVisible(true);
+        f.requestFocusInWindow();
+    }
+
+    public void submit() {
+        // set the text of the label to the text of the field
+        String text = t.getText();
+        main.Commands(text);
+
+        fullText = fullText + text + "<br/>";
+
+        l.setText("<html>" + fullText + "</html>");
+
+        // set the text of field to blank
+        t.setText("");
+
+    }
+
+    // if the button is pressed
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+        if (s.equals("submit")) {
+            submit();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+            submit();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
