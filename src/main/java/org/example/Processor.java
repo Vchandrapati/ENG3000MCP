fd00bbe09d9e19ae7c59a1088facadf71a5c8159
Ashton <ashton.lane1@students.mq.edu.au>
2024-10-08 19:42:18 +1100

    added train misalignment code

--- src/main/java/org/example/Processor.java
+++ src/main/java/org/example/Processor.java
@@ -12,27 +12,30 @@ public class Processor {
     private static int totalBlocks;
     private static final int MAX_BLOCKS = 10;
 
-    private Processor() {}
+    private Processor() {
+    }
 
     public static void checkpointTripped(int checkpointTripped, boolean untrip) {
         SystemStateManager systemStateManager = SystemStateManager.getInstance();
         totalBlocks = db.getCheckpointCount();
 
         if (!isCheckpointValid(checkpointTripped)) {
+            logger.log(Level.WARNING, "Inconsistent checkpoint trip : {0} on trip boolean : {1}",
+                    new Object[] { checkpointTripped, untrip });
             return;
         }
 
         if (systemStateManager.needsTrip(checkpointTripped, untrip)) {
+            logger.log(Level.WARNING, "Thomas to do, not sure what needstrip is");
             return;
         }
 
         // checks if the checkpoint before tripped checkpoint contains a blade runner
         int previousCheckpoint = calculatePreviousBlock(checkpointTripped);
         if (!db.isBlockOccupied(previousCheckpoint)) {
-            String id =
-                    (checkpointTripped > 9) ? "CP" + checkpointTripped : "CP0" + checkpointTripped;
+            String id = (checkpointTripped > 9) ? "CP" + checkpointTripped : "CP0" + checkpointTripped;
             logger.log(Level.WARNING, "Inconsistent checkpoint trip : {0} on trip boolean : {1}",
-                    new Object[] {id, untrip});
+                    new Object[] { id, untrip });
             systemStateManager.addUnresponsiveClient(id, ReasonEnum.INCORTRIP);
         } else {
             handleTrip(checkpointTripped, previousCheckpoint, untrip);
@@ -56,7 +59,8 @@ public class Processor {
             bladeRunnerOptional.get().sendExecuteMessage(MessageEnums.CCPAction.STOPC);
             if (untrip) {
                 String id = bladeRunnerOptional.get().getId();
-                logger.log(Level.WARNING, "Multiple blade runners in the same zone, includes : {0}", id);
+                logger.log(Level.WARNING, "Multiple blade runners in the same zone, includes : {0}",
+                        id);
                 SystemStateManager.getInstance().addUnresponsiveClient(id, ReasonEnum.COLLISION);
             }
             return;
@@ -87,26 +91,28 @@ public class Processor {
         return db.getClient(bladeRunnerID, BladeRunnerClient.class);
     }
 
-    // frees ONE blade runner behind the current, he will subsequently free the rest by moving
+    // frees ONE blade runner behind the current, he will subsequently free the rest
+    // by moving
     private static void checkForTraffic(int checkpoint) {
         // check block behind
         int blockBefore = calculatePreviousBlock(checkpoint);
         if (db.isBlockOccupied(blockBefore)) {
             Optional<BladeRunnerClient> bladeRunnerOptional = getBladeRunner(blockBefore);
-            bladeRunnerOptional.ifPresent(br -> br.sendExecuteMessage(MessageEnums.CCPAction.FSLOWC));
+            bladeRunnerOptional.ifPresent(br -> br.sendExecuteMessage(MessageEnums.CCPAction.FFASTC));
+            bladeRunnerOptional.ifPresent(br -> br.updateStatus(MessageEnums.CCPStatus.FFASTC));
         }
     }
 
     public static int calculatePreviousBlock(int checkpoint) {
         totalBlocks = db.getCheckpointCount();
 
-        while(true) {
+        while (true) {
             checkpoint--;
             if (checkpoint == 0) {
                 checkpoint = MAX_BLOCKS;
             }
-            
-            if(isCheckpointValid(checkpoint)) {
+
+            if (isCheckpointValid(checkpoint)) {
                 return checkpoint;
             }
         }
@@ -115,13 +121,13 @@ public class Processor {
     public static int calculateNextBlock(int checkpoint) {
         totalBlocks = db.getCheckpointCount();
 
-        while(true) {
+        while (true) {
             checkpoint++;
             if (checkpoint == MAX_BLOCKS) {
                 checkpoint = 1;
             }
-            
-            if(isCheckpointValid(checkpoint)) {
+
+            if (isCheckpointValid(checkpoint)) {
                 return checkpoint;
             }
         }
@@ -139,4 +145,38 @@ public class Processor {
 
         return true;
     }
+
+    private static void trainAligned() {
+        // Nothing needed at the moment
+    }
+
+    private static void trainUnaligned(int stationCheckpoint) {
+        Optional<BladeRunnerClient> br = getBladeRunner(stationCheckpoint - 1);
+        Optional<BladeRunnerClient> brOverShot = getBladeRunner(stationCheckpoint);
+
+        // stop bladeRunner behind the station checkpoint incase there is one
+        Optional<BladeRunnerClient> bladeRunnerBehind = getBladeRunner(stationCheckpoint - 2);
+
+        if (bladeRunnerBehind.isPresent()) {
+            bladeRunnerBehind.get().sendExecuteMessage(MessageEnums.CCPAction.STOPC);
+            bladeRunnerBehind.get().updateStatus(MessageEnums.CCPStatus.STOPC);
+        }
+
+        if (br.isEmpty() && brOverShot.isEmpty()) {
+            logger.log(Level.WARNING, "Tried to get blade runner at checkpoint {0} but failed",
+                    stationCheckpoint);
+            return;
+        }
+
+        if (br.isPresent()) {
+            // train has undershot, not our responsibility to align i dont think
+        }
+
+        if (brOverShot.isPresent()) { // train has overshot
+            brOverShot.get().sendExecuteMessage(MessageEnums.CCPAction.RSLOWC);
+            brOverShot.get().updateStatus(MessageEnums.CCPStatus.RSLOWC);
+        }
+
+    }
 }
+
